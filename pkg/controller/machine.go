bc3ace6ceb8d0b9e74f2c2e01d17d6b0140d831c
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2025-04-29 12:17:13 +0300

    Add record event about successful draining node

--- pkg/controller/machine.go
+++ pkg/controller/machine.go
@@ -798,6 +798,7 @@ func (c *controller) machineDelete(machine *v1alpha1.Machine, driver drivers.Dri
 			if err == nil {
 				// Drain successful
 				klog.V(2).Infof("Drain successful for machine %q. \nBuf:%v \nErrBuf:%v", machine.Name, buf, errBuf)
+				c.recorder.Eventf(machine, corev1.EventTypeNormal, "SuccessfulDrainNode", "success draining Machine's node %q", machine.Status.Node)
 
 			} else if err != nil && forceDeleteMachine {
 				// Drain failed on force deletion
